c638bf7bcef68b0dedde89df1e0a1e9a953e3f3f
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-07-10 17:53:27 -0600

    fixed the Os actually

--- java.js
+++ java.js
@@ -1,81 +1,67 @@
-window.onload = start;
-var boxes = document.getElementsByTagName("td");
-var turnText = document.querySelector(".playerTurn");
-var counter = 1;
-var winCounter = 0;
-var OMoves = [];
-var XMoves = [];
+var playerX = {
+  claimedSquares: [],
+  wins: 0
+};
 
-var winningCombos = [[0,1,2],[3,4,5],[6,7,8],
-[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+var playerO = {
+  claimedSquares: [],
+  wins: 0
+};
 
-function start(){
-  addXandOListener();
-  addResetListener();
-}
+var winner = false;
 
-function addXandOListener(){
-  for (var i = boxes.length - 1; i >= 0; i--) {
-    boxes[i].addEventListener("click", addXorO);
-  }
-}
-
-function addXorO(event){
-  if (event.target.innerHTML.length === 0){
-    if (counter % 2 === 0) {
-      OMoves.push(parseInt(event.target.getAttribute("data-num")));
-      event.target.innerHTML = "O";
-      event.target.setAttribute("class","O");
-      turnText.innerHTML = "PLAYER X'S TURN";
-      counter++;
-      checkForWin(OMoves, "O");
-    }
-    else {
-      XMoves.push(parseInt(event.target.getAttribute("data-num")));
-      event.target.innerHTML = "X";
-      event.target.setAttribute("class","X");
-      turnText.innerHTML = "PLAYER O'S TURN";
-      counter++;
-      checkForWin(XMoves, "X");
-    }
-  if (counter >= 10){
-    turnText.innerHTML = "GAME OVER";
-    var conf = confirm("AH HECK! IT'S A DRAW, WANNA PLAY AGAIN?");
-    if(conf){
-      resetBoard();
-    }
-  }
- }
-}
-
-function addResetListener(){
-  var resetButton = document.getElementById("reset");
-  resetButton.addEventListener("click", resetBoard);
-}
-
-function checkForWin(movesArray, name){
-  for (i = 0; i < winningCombos.length; i++) {
-    winCounter = 0;
-    for (var j = 0; j < winningCombos[i].length; j++) {
-      if(movesArray.indexOf(winningCombos[i][j]) !== -1){
-        winCounter++;
-      }
-      if(winCounter === 3){
-        alert("HEY " + name + "YOU WIN!");
-        resetBoard();
+var winningCombinations = /*I have no idea how to store these*/
+  [
+    [1,2,3], 
+    [4,5,6],
+    [7,8,9],
+    [1,4,7], 
+    [2,5,8],
+    [3,6,9],
+    [1,5,9], 
+    [3,5,7]
+  ];
+var turn = 'x';
+function play() {
+  if (turn === 'x') { //If X's turn...
+    $(this).empty();
+    $(this).text('X'); //Place an X
+    playerX.claimedSquares.push(Number($(this).attr('class')));//Add that square to X's array
+    var counter = 0;
+    for(var i = 0; i < winningCombinations.length; i++) {
+      for(var j = 0; j < winningCombinations[i].length; j++) {
+        if (playerX.claimedSquares.indexOf(winningCombinations[i][j]) !== -1) {
+          counter++;
+        }
       }
     }
+    if (counter >= 3) {
+      console.log("X is the winner!");
+    }
+    turn = 'o'; //O's turn!
+    console.log(turn + "'s turn now!");
+  } else if (turn === 'o') { //If O's turn...
+    $(this).empty();
+    $(this).text('O'); //Place an O
+    playerO.claimedSquares.push($(this).attr('class')); //Add that square to O's array
+    turn = 'x';
+    console.log(turn + "'s turn now!");
   }
+  $(this).off();
 }
 
-function resetBoard(){
-  for (var i = boxes.length - 1; i >= 0; i--) {
-    boxes[i].innerHTML="";
-    boxes[i].setAttribute("class","clear");
-  }
-  OMoves = [];
-  XMoves = [];
-  winCounter = 0;
-  counter = 1;
-  turnText.innerHTML = "PLAYER X'S TURN!";
+function clearBoard () {
+  console.log($('td'));
+  $('td').text('');
+  turn = 'x';
+  console.log('RESET! ' + turn + "'s turn now!");
+  $('td').off();
+  $('td').click(play);
+  playerX = {claimedSquares: [], wins: 0};
+  playerY = {claimedSquares: [], wins: 0};
 }
+
+$(document).ready(function() {
+  $('td').click(play);
+  $('button').click(clearBoard);  
+});
